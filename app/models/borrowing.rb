e4da85c963a9230c144329a73c4ca0897d079524
MJCappella <mjaymickey001@gmail.com>
2025-02-14 01:09:46 +0300

    documented the functionality of the model

--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -1,3 +1,5 @@
+# The borrowing model is a simple model that represents a book being borrowed by a borrower.
+
 class Borrowing < ApplicationRecord
   belongs_to :book
   validates :borrower, presence: true
